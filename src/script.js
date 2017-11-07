9002d6c133c7b6d27e082134eb72f3d9535a91d7
Sagar Chamling <sgr.raee@gmail.com>
2017-11-07 10:13:42 +0545

    Array Intro

--- src/script.js
+++ src/script.js
@@ -5,3 +5,11 @@ console.log(heading.toUpperCase());
 console.log(heading.substr(0, 5));
 
 console.log("Hello" + "World");
+
+var fruits = ["Apple", "Mango", "Banana"];
+console.log(fruits);
+console.log(fruits.length);
+
+fruits.push("Tomato")
+console.log(fruits);
+
